78cad9a2c5b4cbcfd9717040f003042406f772f8
Daniel Hodvogner <dhodvogner@gmail.com>
2023-10-20 16:57:47 +0200

    wip: Settings

--- src/settings/renderer.js
+++ src/settings/renderer.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   window.electronAPI.getLogs()
+
+  document.getElementById('refresh-logs').addEventListener('click', () => {
+    window.electronAPI.getLogs()
+  })
+
+  document.getElementById('settings-form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    const formData = new FormData(event.target)
+    console.log(formData)
+
+    // TODO: Save settings
+  })
 })
 
 window.electronAPI.onLogs((logs) => {
